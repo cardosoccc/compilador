78be5a384c7cf74e4b2b6190093c26dca2ef4f63
Caio Cargnin Cardoso <cardoso.ccc@gmail.com>
2016-06-28 09:50:46 -0300

    Corrigindo identação

--- src/View/TelaPrincipal.java
+++ src/View/TelaPrincipal.java
@@ -155,7 +155,7 @@ public class TelaPrincipal {
 		this.textArea.setCaretPosition(e.getPosition());
 		String mensagem = "Encontrado um erro ";
 		mensagem += (e instanceof LexicalError)  ? "léxico" :
-			        (e instanceof SyntaticError) ? "sintático" :
+                    (e instanceof SyntaticError) ? "sintático" :
                     (e instanceof SemanticError) ? "semântico" : "";
 		mensagem += ":\n\n" + "Msg: " + e.getMessage() + "\nPosição: ";
 		this.informeUsuario(mensagem);
